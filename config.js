4b0bfa4c7e0a0e245780006a96ccef64587db04b
charan-kanaparthi <kvenkatcharan@gmail.com>
2021-10-22 20:52:58 +0530

    initial setup 2

--- config.js
+++ config.js
@@ -1,38 +1,36 @@
 'use strict';
-// const dotenv = require('dotenv');
-// const assert = require('assert');
+const dotenv = require('dotenv');
+const assert = require('assert');
 
-// dotenv.config();
+dotenv.config();
 
-// const {
-//     PORT,
-//     HOST,
-//     HOST_URL,
-//     API_KEY,
-//     AUTH_DOMAIN,
-//     DATABASE_URL,
-//     PROJECT_ID,
-//     STORAGE_BUCKET,
-//     MESSAGING_SENDER_ID,
-//     APP_ID
-// } = process.env;
+const {
+    PORT,
+    HOST,
+    HOST_URL,
+    API_KEY,
+    AUTH_DOMAIN,
+    DATABASE_URL,
+    PROJECT_ID,
+    STORAGE_BUCKET,
+    MESSAGING_SENDER_ID,
+    APP_ID
+} = process.env;
 
-// assert(PORT, 'PORT is required');
-// assert(HOST, 'HOST is required');
+assert(PORT, 'PORT is required');
+assert(HOST, 'HOST is required');
 
 module.exports = {
-    port: 8080,
-    host: "localhost",
-    url: "http://localhost:8080",
-    
+    port: PORT,
+    host: HOST,
+    url: HOST_URL,
     firebaseConfig: {
-    apiKey: "AIzaSyD0hWcsSNCMmNXVy8LP9NkmjL-QELnizEs",
-    authDomain: "fir-a43f5.firebaseapp.com",
-    databaseURL: "https://fir-a43f5-default-rtdb.firebaseio.com",
-    projectId: "fir-a43f5",
-    storageBucket: "fir-a43f5.appspot.com",
-    messagingSenderId: "589443950538",
-    appId: "1:589443950538:web:ce76d134d6f0bc1f109133",
-    measurementId: "G-TW502YKYSX"
+        apiKey: API_KEY,
+        authDomain: AUTH_DOMAIN,
+        databaseURL: DATABASE_URL,
+        projectId: PROJECT_ID,
+        storageBucket: STORAGE_BUCKET,
+        messagingSenderId: MESSAGING_SENDER_ID,
+        appId: APP_ID
     }
 }
